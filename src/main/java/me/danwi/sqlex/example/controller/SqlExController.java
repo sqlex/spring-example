e47b8398b0736b6a3bb6b0430f928e0e8be7f8ae
李雨田 <demon@danwi.me>
2022-04-21 18:02:53 +0800

    添加了分页查询和单行查询的例子

--- src/main/java/me/danwi/sqlex/example/controller/SqlExController.java
+++ src/main/java/me/danwi/sqlex/example/controller/SqlExController.java
@@ -1,5 +1,6 @@
 package me.danwi.sqlex.example.controller;
 
+import me.danwi.sqlex.core.type.PagedResult;
 import me.danwi.sqlex.example.dao.DepartmentDao;
 import me.danwi.sqlex.example.dao.PersonDao;
 import me.danwi.sqlex.spring.SpringDaoFactory;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.stream.Collectors;
+import java.util.List;
 
 @RestController
 public class SqlExController {
@@ -17,8 +18,13 @@ public class SqlExController {
     @Autowired
     private SpringDaoFactory factory;
 
-    @Transactional
     @RequestMapping("/")
+    public PagedResult<PersonDao.AllPerson> getAll(@RequestParam("pageSize") long pageSize, @RequestParam("pageNo") long pageNo) {
+        return factory.getInstance(PersonDao.class).findAllPerson(pageSize, pageNo);
+    }
+
+    @Transactional
+    @RequestMapping("/add")
     public void addPerson() {
         //通过factory获取到数据访问对象
         DepartmentDao departmentDao = factory.getInstance(DepartmentDao.class);
@@ -27,11 +33,14 @@ public class SqlExController {
         departmentDao.addDepartment(0, "dev2"); //第二次执行会主键冲突,整体回滚
     }
 
+    @RequestMapping("/get")
+    public PersonDao.Person getPerson(@RequestParam("id") int id) {
+        return factory.getInstance(PersonDao.class).findPerson(id);
+    }
+
     @RequestMapping("/count")
-    public String getCountOfDepartment(@RequestParam("name") String name) {
+    public List<PersonDao.DepartmentAmount> getCountOfDepartment(@RequestParam("name") String name) {
         return factory.getInstance(PersonDao.class)
-                .findAmountOfDepartment(name)
-                .stream().map(it -> it.getDepartmentName() + " : " + it.getAmount())
-                .collect(Collectors.joining(" - "));
+                .findAmountOfDepartment(name);
     }
 }
